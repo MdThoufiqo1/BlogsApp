263a1c8bb4834fb9ca4839112d0dfb0803930d77
MdThoufiqo1 <mdthoufiq106@gmail.com>
2024-09-15 13:11:52 +0530

    ready

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ require('dotenv').config()
 const app=express()
 app.use(express.json())
 app.use(cors({
-    origin:true,
+    origin:["http://localhost:5173/"],
     methods:["GET","POST","PUT","DELETE"],
     credentials:true
 }))
